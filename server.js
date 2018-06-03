de151e1969fddf6eeb73ed6d0a25bd1fe1c18019
Heikki <heikki.metsola@helsinki.fi>
2018-06-03 10:19:45 +0300

    edits
    Added photo

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express')
 var app = express()
 
+
+
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views');
 //app.use(express.static(__dirname, 'public'));
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.render('index.pug')
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8000!')
+  console.log('Example app listening on port 8080!')
 })
