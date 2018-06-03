4dc63aed8bf6dba80cc710b433ddba8551918112
Heikki <heikki.metsola@helsinki.fi>
2018-06-03 14:06:33 +0300

    muutoksia, game destination
    ++

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ app.set('view engine', 'pug')
 app.set('views', __dirname + '/views');
 //app.use(express.static(__dirname, 'public'));
 app.use(express.static('public'))
+app.use(express.static('styles'))
+app.use(express.static('destination'))
 
 app.get('/', function (req, res) {
-  res.render('index.pug')
+  res.render('index')
 })
 
+app.get('/newpage', function(req, res){
+  res.render('game', {
+    title: 'Home'
+  });
+});
+
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Kalapeli running on port 8080!')
 })
